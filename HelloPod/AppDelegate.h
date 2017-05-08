6709a57608c13b7c683fb5b0df3489a2a6137187
alephs <alephs@alephss-MacBook-Air.local>
2017-05-08 08:34:04 +0530

    Initial Commit

---
+++ HelloPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloPod
+//
+//  Created by alephs on 08/05/17.
+//  Copyright © 2017 aleph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloPod
+//
+//  Created by alephs on 08/05/17.
+//  Copyright © 2017 aleph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
